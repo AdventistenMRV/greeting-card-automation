4d4d74a7e2561baf25d0a40b614205617bb054e0
Ruben Klepp <ruben@klepp.media>
2021-11-23 14:04:47 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1>Hello World</h1>
+    <iframe :src="base64PDF" v-show="showPDF"></iframe>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import GreetingCard from "../classes/GreetingCard";
+//import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+  data() {
+    return {
+      card: new GreetingCard(),
+      showPDF: false,
+      base64PDF: "",
+    };
+  },
+  components: {},
+  async mounted() {
+    this.card.sayHello("Vivian");
+    await this.card.createPdf().then((status) => {
+      this.showPDF = true;
+      console.log(status);
+    });
+  },
+});
+</script>
